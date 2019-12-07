6b29344382a89187c4de0d570ea8a1b2d637caed
Arribdi.L@gmail.com <arribdi.l@gmail.com>
2019-12-07 15:01:39 +0300

    refactor: removing duplicated lines
    remove uunnecessary print for the column month and delete the line of reassinging day column.
    
    deleted lines: 72, 76

--- bikeshare.py
+++ bikeshare.py
@@ -69,12 +69,10 @@ def time_stats(df):
     start_time = time.time()
 
     # TO DO: display the most common month
-    print(df['month'].head())
     common_month = df['month'].mode()[0]
     print('the most common month:', common_month)
     
     # TO DO: display the most common day of week
-    df['day'] = df['Start Time'].dt.day
     common_day = df['day'].mode()[0]
     print('the most common day:', common_day)
     
